76021379a7dac911588bfb775430803e68b27698
root <2987496806@qq.com>
2021-01-04 10:44:37 +0800

    enter

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Artictles
 # Register your models here.
+
+class ArticlesAdmin(admin.ModelAdmin):
+     list_display=("")
+     search_fields=("")
+     list_filter=("")
+
+
+
+admin.site.register(Articles,ArticlesAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Articles(models.Model):
+     title = models.CharField(max_length=128,verbose_name="文章标题")
+     author = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name="文章作者")
+     img = models.ImageField(upload_to="",verbose_name="文章配图")
+     abstract = models.TextField(verbose_name="文章摘要")
+     content = models.TextField(verbose_name="文章内容")
+     visited = models.IntegerField(verbose_name="文章访问量")
+     created_at = models.DateTimeField(auto_now_add=True,verbose_name="创建时间")
+     modified_at = models.DateTimeField(auto_now=True,verbose_name="修改时间")
+
+    
+    
